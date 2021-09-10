f91cfc237fecc7a61a79e77a01c2d535121d387d
sneiky <csgoup58@gmail.com>
2021-09-10 20:42:16 +0300

    menushka

---
+++ menu.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("sidebar").classList.toggle('active');
+}
+//document.querySelector(".btn").addEventListener('click', function (e) {
+//e.preventDefault();
+//this.classList.toggle('is-active');
+//})
